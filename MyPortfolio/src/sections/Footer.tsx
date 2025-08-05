f76291c49f1e64ac369064f1a579ab3e27d713a1
fenohery <fenohery@localhost.localdomain>
2025-08-05 16:17:42 +0300

    04/08 adding logo on footer

--- MyPortfolio/src/sections/Footer.tsx
+++ MyPortfolio/src/sections/Footer.tsx
@@ -1,7 +1,12 @@
+import Logo from '/Logo.svg';
+
 const Footer = () => {
     return (
         <footer className="flex items-center justify-center border-t-1">
-            <p className="font-bold capitalize">designed by me | fenohery</p>
+            <p className="font-bold capitalize">designed by me | <span className='font-black'>fenohery</span></p>
+            <figure className="w-8 h-8">
+                <img src={Logo} alt="Logo" className="w-full h-full object-contain" />
+            </figure>
         </footer>
     )
 }
